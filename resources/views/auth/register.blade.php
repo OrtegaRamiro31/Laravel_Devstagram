912c3f32d0abf75422be28c4c71f548aad43b11b
ramiro <ceinnova13458@innovaccion.mx>
2023-06-04 17:16:47 -0600

    Bugs la vista al validar el formulario reparados.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,8 +37,8 @@
                         name="username"
                         type="text"
                         placeholder="Tu Nombre de Usuario"
-                        class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
-                        value="{{old('name') }}"
+                        class="border p-3 w-full rounded-lg @error('username') border-red-500 @enderror"
+                        value="{{old('username') }}"
                     />
                     @error('username')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
@@ -53,8 +53,8 @@
                         name="email"
                         type="email"
                         placeholder="Tu Email de Registro"
-                        class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
-                        value="{{old('name') }}"
+                        class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
+                        value="{{old('email') }}"
                     />
                     @error('email')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
