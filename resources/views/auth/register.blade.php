1b95740cc690543f249d76c6a2babdc257f64292
ramiro <ceinnova13458@innovaccion.mx>
2023-06-04 15:55:53 -0600

    Formulario creado en app.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,3 +3,77 @@
 @section('titulo')
     Regístrate en DevStagram
 @endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center">
+        <div class="md:w-1/2">
+            <p>Imágen aquí</p>
+        </div>
+        <div class="md:w-4/12">
+            <form>
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre
+                    </label>
+                    <input
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Tu Nombre"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Tu Nombre de Usuario"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Tu Email de Registro"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Password
+                    </label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Password de Registro"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Confirmar Password
+                    </label>
+                    <input
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        type="password"
+                        placeholder="Confirma tu password"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+
+                <input type="submit" value="Crear Cuenta" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
+@endsection
