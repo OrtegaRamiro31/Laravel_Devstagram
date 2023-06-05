94ea831e764bc76778e6f792baf94e2874965cba
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-04 22:15:21 -0600

    Mantener sesión iniciada agregado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,10 @@
                                 {{auth()->user()->username}}
                             </span>
                         </a>
-                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{route('register')}}">Cerrar Sesión</a>
+                        <form action="{{route('logout')}}" method="POST">
+                            @csrf
+                            <button type="submit" class="font-bold uppercase text-gray-600 text-sm">Cerrar Sesión</button>
+                        </form>
                     </nav>
                 @endauth
                 @guest
