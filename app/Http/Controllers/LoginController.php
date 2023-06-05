6a60dbdaf34d862eddd8173ffc7bd6a7bb8afaea
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-04 21:53:30 -0600

    Validación para inicio de sesión agregado.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,7 +7,16 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
         return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'  
+        ]);
     }
 }
