783a0c1b74ad56cd0fb3697cea5a1ede7fdc299c
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-05 19:59:04 -0600

    Dropzone para imágenes agregado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>DevStagram - @yield('titulo')</title>
         @vite('resources/css/app.css')
+        @vite('resources/js/app.js')
     </head>
     <body class="bg-gray-100">
         <header class="p-5 border bg-white shadow">
