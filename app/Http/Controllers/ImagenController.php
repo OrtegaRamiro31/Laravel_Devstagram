866fc5aec3a7fad7554922ca3dc2681358e2806f
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-05 20:20:50 -0600

    Solucionado error de subida de imágen.

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 class ImagenController extends Controller
 {
     //
-    public function store(){
-        return ('Desde imagen controler');
+    public function store(Request $request){
+        $imagen = $request->file('file');
+
+        return response()->json(['imagen' => $imagen->extension()]);
     }
 }
