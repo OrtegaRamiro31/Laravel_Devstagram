5b34a98d801394bea99ac56ba21f15f36939655d
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-07 18:50:53 -0600

    Agregada nueva ruta POST para almacenar comentarios

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -61,6 +61,7 @@ class PostController extends Controller
     public function show(User $user,Post $post){
         return view('posts.show', [
             'post'=> $post,
+            'user' => $user
         ]);
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('contenido')
-    <div class="container mx-auto flex">
+    <div class="container mx-auto md:flex">
         <div class="md:w-1/2">
             <img src="{{ asset('uploads') . '/' . $post->imagen}}" alt="Imagen del post {{$post->titulo}}"/>
 
@@ -23,7 +23,8 @@
 
                 @auth  
                     <p class="text-xl font-bold text-center mb-4">Agrega un Nuevo Comentario</p>
-                    <form>
+                    <form action="{{ route('comentarios.store', ['post' => $post, 'user' => $user] ) }}" method="POST">
+                        @csrf
                         <div class="mb-5">
                             <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
                                 Añade un Comentario
