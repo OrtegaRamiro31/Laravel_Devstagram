3fd1462062e4af65fb8a48b75c4b874425f03f7b
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-07 20:05:36 -0600

    Eliminación de posts agregada a la lógica

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
@@ -66,6 +67,16 @@ class PostController extends Controller
     }
 
     public function destroy(Post $post) {
-        dd('Eliminando ', $post->id);
+        $this->authorize('delete', $post);
+        $post->delete();
+
+        // Eliminar la imágen
+        $imagen_path = public_path('uploads/' . $post->imagen);
+
+        if(File::exists($imagen_path)) {
+            unlink($imagen_path);
+        }
+
+        return redirect()->route('posts.index', auth()->user()->username); 
     }
 }
