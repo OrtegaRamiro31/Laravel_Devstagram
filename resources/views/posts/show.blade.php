4ea9fb60c7f3ba7b919a41bc9423970711722ed2
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-07 20:28:23 -0600

    Funcionalidad para solamente dar like a post y almacenarlo en la BD añadida

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,7 +10,8 @@
             <img src="{{ asset('uploads') . '/' . $post->imagen}}" alt="Imagen del post {{$post->titulo}}"/>
 
             <div class="p-3 flex items-center gap-4">
-                <form action="">
+                <form method="POST" action="{{route('posts.likes.store', $post)}}">
+                    @csrf
                     <div class="my-4">
                         <button type="submit">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
