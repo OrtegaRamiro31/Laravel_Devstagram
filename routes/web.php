84b359f6b6d984068cd4e2a3b34904ebe6a4ccfb
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-08 18:56:21 -0600

    Rutas agregadas para mostrar vista y hacer petición POST sobre el perfil del usuario agregadas.

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
@@ -44,3 +45,7 @@ Route::post('imagenes', [ImagenController::class, 'store'])->name('imagenes.stor
 // Like
 Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
 Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
+// Perfil de usuario
+Route::get('{user:username}/editar-perfil', [PerfilController::class, 'index'])->name('perfil.index');
+Route::post('{user:username}/editar-perfil', [PerfilController::class, 'store'])->name('perfil.store');
