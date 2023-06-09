eca14dfd28cb9715b55c1d604b2658c0f2651a12
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-09 16:33:05 -0600

    Componente de livewire like-post agregado en show.blade.php. Button agregado con evento click agregado en like-post.blade.php. Función de prueba like() agregada en LikePost.php

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class LikePost extends Component
 {
+    public $post;
+
+    public function like()
+    {
+        return "desde fun like";
+    }
+
     public function render()
     {
         return view('livewire.like-post');
