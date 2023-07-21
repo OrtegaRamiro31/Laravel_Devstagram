7f08ae05f9008b6f3f78710e74d6e0c0502fc73d
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-07-21 09:14:46 -0600

    Mejorado responsive design en la navegación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,8 @@
 
 
                 @auth
-                    <nav class="flex flex-col lg:flex-row gap-2 items-center">
-                        <a href="{{route('conocer')}}" class="text-center font-bold hover:text-gray-600 mt-4 lg:mt-0 lg:mr-8">¡¡Mira los posts de otras personas!!</a>
+                    <nav class="flex flex-col md:flex-row gap-2 items-center md:mt-4 lg:mt-0">
+                        <a href="{{route('conocer')}}" class="text-center font-bold hover:text-gray-600 mt-4 md:mt-0 md:mr-8">¡¡Mira los posts de otras personas!!</a>
                         <a href="{{route('posts.create')}}" class="flex items-center gap-2 bg-white border p-2 text-gray-600 rounded text-sm uppercase font-bold cursor-pointer">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
